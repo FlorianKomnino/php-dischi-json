2cf0fda8a48ff5f6ff2763365587787e80843b95
FlorianKomnino <florian.komnino@outlook.it>
2023-01-27 17:11:20 +0100

    visualization of json on server done!

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <!-- * Vue -->
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js" type="text/javascript" defer></script>
+    <!-- * Axios-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.2.2/axios.min.js" integrity="sha512-QTnb9BQkG4fBYIt9JGvYmxPpd6TBeKp6lsUrtiVQsrJ9sb33Bn9s0wMQO9qVBFbPX3xHRAsBHvXlcsrnJjExjg==" crossorigin="anonymous" referrerpolicy="no-referrer" type="text/javascript" defer></script>
+    <link rel="icon" type="image/svg+xml" href="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/2048px-Spotify_logo_without_text.svg.png" />
+    <title>php-dischi-json</title>
+</head>
+
+<body>
+    <?php ?>
+</body>
+
+</html>
---
+++ server.php
@@ -0,0 +1,5 @@
+<?php 
+
+$jsonData = file_get_contents(__DIR__ . '/db/dischi.json');
+
+echo $jsonData;
